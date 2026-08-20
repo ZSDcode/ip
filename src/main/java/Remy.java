827e2332365703442e4e3e893691b8ef15e6d053
ZSDcode <dylantayzyyt@gmail.com>
2026-08-20 11:41:13 +0800

    Change name to Remy and add greet and goodbye text

---
+++ src/main/java/Remy.java
@@ -0,0 +1,19 @@
+public class Remy {
+    public static void main(String[] args) {
+        String banner = " ____                    \n"
+            + "|  _ \\  ___  _ __ ___  _   _ \n"
+            + "| |_) |/ _ \\| '_ ` _ \\| | | |\n"
+            + "|  _ <|  __/| | | | | | |_| |\n"
+            + "|_| \\_\\\\___||_| |_| |_|\\__, |\n"
+            + "                       |___/ \n";
+        System.out.println(banner);
+        String line = "_______________________________________________\n";
+        String Greeting = line
+            + "Hello! I'm Remy, your friendly reminder app! \n"
+            + "How can I help you today?\n"
+            + line;
+        System.out.println(Greeting);
+        String Goodbye = "Bye, enjoy your day!! \n" + line;
+        System.out.println(Goodbye);
+    }
+}
